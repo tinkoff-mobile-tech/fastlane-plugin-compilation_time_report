cec0190fe99539af42790135531ed019e420dd00
i.v.vasilenko <i.v.vasilenko@tinkoff.ru>
2019-01-29 13:28:21 +0300

    Added test project for example

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by i.v.vasilenko on 29/01/2019.
+//  Copyright © 2019 АО «Тинькофф Банк», лицензия ЦБ РФ № 2673. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let value: String? = nil
+        _ = value ?? value ?? value ?? value ?? value ?? value
+    }
+}
+
